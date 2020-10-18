d3c15e0430722ded43145e640142417597940acf
Alison Reilly <reillyamr@gmail.com>
2020-10-18 14:21:31 -0400

    add api view exp with weather response, export app

--- functions/index.js
+++ functions/index.js
@@ -20,6 +20,13 @@ const app = express();
 const cors = require('cors')({origin: true});
 app.use(cors);
 
+//axios
+const axios = require('axios');
+
+//env
+require('dotenv').config();
+require('dotenv').config({path: '../.env'})
+
 
 
 
@@ -32,9 +39,36 @@ app.get('/', (req, res) => {
         <title>Weather</title>
       </head>
       <body>
-        <p>A Weather Experiment:
+        <p>A Weather Experiment, a page for example:
           <span id="date">${date}</span></p>
         <button onClick="refresh(this)">Refresh</button>
       </body>
     </html>`);
   });
+
+
+  app.get('/miamiweather', (req, res) => {
+    const key = process.env.API_KEY
+    let data = '';
+
+    const config = {
+      method: 'get',
+      url: `http://api.weatherapi.com/v1/current.json?key=${key}&q=Miami`,
+      headers: { 
+        'Content-Type': 'application/json'
+      }
+    };
+    
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      res.json({weather: response.data})
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    
+  });
+
+  exports.app = functions.https.onRequest(app);
+
