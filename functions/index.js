7f17d6533cabe17ddc98448bc3fc30397fba732e
Alison Reilly <reillyamr@gmail.com>
2020-10-18 13:53:05 -0400

    add firestore, express, cors incl., and basic exp

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,34 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+//firestore
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+//express
+const express = require('express');
+const app = express();
+
+//cors
+const cors = require('cors')({origin: true});
+app.use(cors);
+
+
+
+
+app.get('/', (req, res) => {
+    const date = new Date();
+
+    res.send(`
+      <!doctype html>
+      <head>
+        <title>Weather</title>
+      </head>
+      <body>
+        <p>A Weather Experiment:
+          <span id="date">${date}</span></p>
+        <button onClick="refresh(this)">Refresh</button>
+      </body>
+    </html>`);
+  });
